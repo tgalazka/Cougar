95902c44d26101749ddba55666d8e328da22751e
Thomas Galazka <tjgalazka@gmail.com>
2011-04-13 09:01:36 -0400

    Renamed a folder and modified Cougar

--- Cougar.pm
+++ Cougar.pm
@@ -118,12 +118,19 @@ sub steps {
 }
 
 
-#######################
-# Handle the features #
-#######################
+###################
+# Main: Features #
+##################
 sub run_features {
   my $self  = shift;
-  my $files_ref = $self->collect_feature_files;
+  $self->collect_feature_files;
+  foreach (@$self->feature_files) {
+    my $feature = Feature->new;
+    $feature->feature_file($_);
+    $feature->open_feature;
+    $feature->read_feature;
+    $feature->run;
+  }
 }
 
 sub collect_feature_files {
@@ -169,4 +176,117 @@ sub prepend_features {
   my $self  = shift;
   my $file  = shift;
   return $self->features."/$file";
+}
+
+
+package Cougar::Feature;
+
+use vars qw( $PARAMS $SCENARIO );
+
+sub new {
+  $PARAMS = {
+      _FILE           => undef
+    , _FH             => undef
+    , _FH_ACCESSIBLE  => undef
+    , _ACTIVE         => undef
+    , _HEADER         => {
+      _TEXT => undef
+    }
+  };
+  return bless {};
+}
+
+sub trim {
+  my $line  = shift;
+  $line =~ s/^\s+//;
+  $line =~ s/\s+$//;
+  return $line;
+}
+sub _param {
+  my $self    = shift;
+  my $param   = shift;
+  my $value   = shift;
+  my $default = shift;
+  
+  $PARAMS->{$param} = $value if defined $value;
+  return $PARAMS->{$param} if defined $PARAMS->{$param};
+  return $PARAMS->{$param} = ( (defined $value) ? $value : $default );
+}
+
+sub feature_file {
+  my $self  = shift;
+  my $file  = shift;
+  return $self->_param( '_FILE', $file, undef );
+}
+
+sub feature_file_handle {
+  my $self  = shift;
+  my $fh    = shift;
+  return $self->_param( '_FH', $fh, undef );
+}
+
+sub is_accessible {
+  my $self  = shift;
+  my $open  = shift;
+  return $self->_param( "_FH_ACCESSIBLE", $open, 0 );
+}
+
+sub open_feature {
+  my $self  = shift;
+  my $file  = shift;
+  $self->feature_file($file) if $file;
+  
+  my $open = open(my $FH, "<", $self->feature_file);
+  $self->feature_file_handle($FH);
+  return $self->is_accessible($open)
+}
+
+sub read_feature {
+  my $self  = shift;
+  return 0 unless $self->is_accessible;
+  
+  my $fh = $self->feature_file_handle;
+  my $section;
+  while( <$fh> ) {
+    my $line = trim($_);
+    next if ($line eq '');
+    
+    if( $self->is_feature($line) ) {
+      $section = 'push_feature';
+    }
+    
+    if( $self->is_scenario($line) ) {
+      $section = 'push_scenario'
+    }
+    
+    next if $section eq 'push_scenario';
+    $self->$section($line);
+  }
+  return $PARAMS;
+}
+
+sub is_feature {
+  my $self  = shift;
+  my $line  = shift;
+  return ($line =~ m/^\s*Feature:.*$/i) ? 'feature' : undef;
+}
+
+sub is_scenario {
+  my $self  = shift;
+  my $line  = shift;
+  return ($line =~ m/^\s*Scenario:.*$/i) ? 'scenario' : undef;
+}
+
+sub publish_feature {
+  my $self  = shift;
+  return $PARAMS->{_HEADER}->{_TEXT};
+}
+
+sub push_feature {
+  my $self  = shift;
+  my $line  = shift;
+  $line = "$line\n";
+  
+  return $PARAMS->{_HEADER}->{_TEXT} = $line unless defined $PARAMS->{_HEADER}->{_TEXT};
+  return $PARAMS->{_HEADER}->{_TEXT} .= "  $line";
 }
